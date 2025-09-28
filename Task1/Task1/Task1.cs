a399df5c0e23c76feb59420ca8b02ba6dd131c40
Kirusha2004 <yodas7212@gmail.com>
2025-09-28 17:34:16 +0300

    Использовал метод Zip()

--- Task1/Task1/Task1.cs
+++ Task1/Task1/Task1.cs
@@ -5,7 +5,7 @@ namespace UserCollections1;
 public sealed class MonthsInYear : IEnumerable<Month>
 {
     private readonly int _year;
-    private static readonly string[] RussianMonthNames =
+    private static readonly IReadOnlyCollection<string> RussianMonthNames =
     [
         "Январь",
         "Февраль",
@@ -31,14 +31,13 @@ public sealed class MonthsInYear : IEnumerable<Month>
 
     public IEnumerator<Month> GetEnumerator()
     {
-        foreach (int monthNumber in Enumerable.Range(1, 12))
-        {
-            yield return new Month(
-                monthNumber,
-                RussianMonthNames[monthNumber - 1],
-                DateTime.DaysInMonth(_year, monthNumber)
-            );
-        }
+        IEnumerable<int> daysInMonths = Enumerable.Range(1, 12)
+            .Select(month => DateTime.DaysInMonth(_year, month));
+
+        return RussianMonthNames
+            .Zip(Enumerable.Range(1, 12), (name, number) => (name, number))
+            .Zip(daysInMonths, (first, days) => new Month(first.number, first.name, days))
+            .GetEnumerator();
     }
 
     IEnumerator IEnumerable.GetEnumerator()
