b74962fda74b505a7608814c2382deceae96b2ee
Kirusha2004 <yodas7212@gmail.com>
2025-09-28 15:29:02 +0300

    Реализация MonthsInYear
    - Создал класс MonthsInYear, реализующий IEnumerable<Month>
    - Добавил конструктор с годом для корректного подсчета дней в феврале
    - Использовал массив русских названий месяцев

--- Task1/Task1/Task1.cs
+++ Task1/Task1/Task1.cs
@@ -1,40 +1,48 @@
-using UserCollections1;
+using System.Collections;
 
-List<Month> list =
-[
-    new Month(1, "Январь", 31),
-    new Month(2, "Февраль", 28),
-    new Month(3, "Март", 31),
-    new Month(4, "Апрель", 30),
-    new Month(5, "Май", 31),
-    new Month(6, "Июнь", 30),
-    new Month(7, "Июль", 31),
-    new Month(8, "Август", 31),
-    new Month(9, "Сентябрь", 30),
-    new Month(10, "Октябрь", 31),
-    new Month(11, "Ноябрь", 30),
-    new Month(12, "Декабрь", 31),
-];
+namespace UserCollections1;
 
-IEnumerable<Month> monthByNumber = list.Where(m => m.Number == 5);
-Console.WriteLine("Месяц по номеру 5");
-foreach (Month? month in monthByNumber)
+public sealed class MonthsInYear : IEnumerable<Month>
 {
-    Console.WriteLine($"{month.Number:00}. {month.Name} - {month.Days} дней");
-}
-Console.WriteLine();
+    private readonly int _year;
+    private static readonly string[] RussianMonthNames =
+    [
+        "Январь",
+        "Февраль",
+        "Март",
+        "Апрель",
+        "Май",
+        "Июнь",
+        "Июль",
+        "Август",
+        "Сентябрь",
+        "Октябрь",
+        "Ноябрь",
+        "Декабрь",
+    ];
 
-IEnumerable<Month> monthsWith31Days = list.Where(m => m.Days == 31);
-Console.WriteLine("Месяцы с 31 днем");
-foreach (Month? month in monthsWith31Days)
-{
-    Console.WriteLine($"{month.Number:00}. {month.Name} - {month.Days} дней");
-}
-Console.WriteLine();
+    public MonthsInYear()
+        : this(DateTime.Now.Year) { }
 
-IEnumerable<Month> combinedResult = list.Where(m => m.Number > 6 && m.Days == 30);
-Console.WriteLine("Месяцы после июня с 30 днями");
-foreach (Month? month in combinedResult)
-{
-    Console.WriteLine($"{month.Number:00}. {month.Name} - {month.Days} дней");
+    public MonthsInYear(int year)
+    {
+        _year = year;
+    }
+
+    public IEnumerator<Month> GetEnumerator()
+    {
+        foreach (int monthNumber in Enumerable.Range(1, 12))
+        {
+            yield return new Month(
+                monthNumber,
+                RussianMonthNames[monthNumber - 1],
+                DateTime.DaysInMonth(_year, monthNumber)
+            );
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
 }
