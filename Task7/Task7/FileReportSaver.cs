c1ab508d97ce3ae9b31613cec04ccc4116117724
Kirusha2004 <yodas7212@gmail.com>
2025-10-06 22:31:25 +0300

    FileReportSaver
    - Сохранение в файл с UTF-8
    - Обработка ошибок

---
+++ Task7/Task7/FileReportSaver.cs
@@ -0,0 +1,21 @@
+using System.Text;
+
+namespace Task7;
+
+public class FileReportSaver : IReportSaver
+{
+    public void SaveReport(string content, string fileName)
+    {
+        try
+        {
+            using (StreamWriter writer = new StreamWriter(fileName, false, Encoding.UTF8))
+            {
+                writer.Write(content);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка при создании файла: {ex.Message}");
+        }
+    }
+}
