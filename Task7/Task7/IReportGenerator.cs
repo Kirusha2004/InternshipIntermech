74ee5482a5e2c840fac220f274509bf4e3d6128c
Kirusha2004 <yodas7212@gmail.com>
2025-10-06 22:30:59 +0300

    интерфейсы для отчетов
    - IReportGenerator для генерации
    - IReportSaver для сохранения

---
+++ Task7/Task7/IReportGenerator.cs
@@ -0,0 +1,6 @@
+namespace Task7;
+
+public interface IReportGenerator
+{
+    string GenerateReport(IList<Product> products);
+}
---
+++ Task7/Task7/IReportSaver.cs
@@ -0,0 +1,6 @@
+namespace Task7;
+
+public interface IReportSaver
+{
+    void SaveReport(string content, string fileName);
+}
