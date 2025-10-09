d5143f486bc840ed629d6ffc2b0a9483f84d59f0
Kirusha2004 <yodas7212@gmail.com>
2025-10-09 22:25:15 +0300

    TemperatureReflectionService для работы с рефлексией
    - Реализована конвертация температур через рефлексию
    - Добавлены методы для получения информации о типах и методах
    - Включена комплексная обработка ошибок
    - Поддержка конвертации Цельсий в Фаренгейт через рефлексию

---
+++ Task10/ReflectionTaskExtentionProject/TemperatureReflectionService.cs
@@ -0,0 +1,45 @@
+using System.Reflection;
+
+namespace ReflectionTaskExtentionProject;
+
+public class TemperatureReflectionService
+{
+    private readonly object _converterInstance;
+    private readonly MethodInfo _conversionMethod;
+
+    public TemperatureReflectionService(Assembly assembly)
+    {
+        Type? type = assembly.GetType("Task10.TemperatureConverter")
+            ?? throw new InvalidOperationException("Класс Task10.TemperatureConverter не найден в сборке");
+
+        _converterInstance = Activator.CreateInstance(type)
+            ?? throw new InvalidOperationException("Не удалось создать экземпляр TemperatureConverter");
+
+        _conversionMethod = type.GetMethod("CelsiusToFahrenheit")
+            ?? throw new InvalidOperationException("Метод CelsiusToFahrenheit не найден");
+    }
+
+    public double ConvertCelsiusToFahrenheit(double celsius)
+    {
+        try
+        {
+            object[] parameters = { celsius };
+            object? result = _conversionMethod.Invoke(_converterInstance, parameters);
+            return (double)result;
+        }
+        catch (Exception e)
+        {
+            throw new InvalidOperationException($"Ошибка при вызове метода конвертации: {e.Message}", e);
+        }
+    }
+
+    public MethodInfo GetConversionMethodInfo()
+    {
+        return _conversionMethod;
+    }
+
+    public Type GetConverterType()
+    {
+        return _converterInstance.GetType();
+    }
+}
