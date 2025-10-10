a584b1e66af7b61e74bec59c48bad3f27fadb456
Kirusha2004 <yodas7212@gmail.com>
2025-10-10 23:12:46 +0300

    фикс ошибок с модификатором

--- Task6/Task6/IFileManager.cs
+++ Task6/Task6/IFileManager.cs
@@ -2,7 +2,7 @@ namespace Task6;
 
 public interface IFileManager
 {
-    string FindFile(string fileName, string? searchPath = null);
-    string ViewFile(string path);
-    void CompressFile(string sourcePath, string? targetPath = null);
+    public string FindFile(string fileName, string? searchPath = null);
+    public string ViewFile(string path);
+    public void CompressFile(string sourcePath, string? targetPath = null);
 }
