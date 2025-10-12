95396eccefdde1c56b39c6f3e766ae94834dc305
Kirusha2004 <yodas7212@gmail.com>
2025-10-12 18:29:46 +0300

    Изменены свойства на init-only Добавлены Equals, GetHashCode, ToString

--- Task5/Task5.Tests/Person.cs
+++ Task5/Task5.Tests/Person.cs
@@ -2,6 +2,21 @@ namespace Task5.Tests;
 
 public class Person
 {
-    public int Id { get; set; }
-    public required string Name { get; set; }
+    public int Id { get; init; }
+    public required string Name { get; init; }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is Person person && Id == person.Id;
+    }
+
+    public override int GetHashCode()
+    {
+        return Id.GetHashCode();
+    }
+
+    public override string ToString()
+    {
+        return $"Person(Id: {Id}, Name: {Name})";
+    }
 }
