1e0824a15693d062f62c8d021e124c55ca75864f
Kirusha2004 <yodas7212@gmail.com>
2025-10-13 14:22:04 +0300

    Убрал try/catch

--- Task7/Task7/FileReportSaver.cs
+++ Task7/Task7/FileReportSaver.cs
@@ -6,14 +6,7 @@ public class FileReportSaver : IReportSaver
 {
     public void SaveReport(string content, string fileName)
     {
-        try
-        {
-            using StreamWriter writer = new StreamWriter(fileName, false, Encoding.UTF8);
-            writer.Write(content);
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"Ошибка при создании файла: {ex.Message}");
-        }
+        using StreamWriter writer = new StreamWriter(fileName, false, Encoding.UTF8);
+        writer.Write(content);
     }
 }
