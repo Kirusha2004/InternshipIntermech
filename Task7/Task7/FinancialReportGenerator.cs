ab86071417e4e0b8448fc4057f210bf31b165152
Kirusha2004 <yodas7212@gmail.com>
2025-10-13 14:25:55 +0300

    Изменил уровень абстракции

--- Task7/Task7/FinancialReportGenerator.cs
+++ Task7/Task7/FinancialReportGenerator.cs
@@ -1,64 +1,38 @@
 using System.Globalization;
-using System.Text;
 
 namespace Task7;
 
 public class FinancialReportGenerator : IReportGenerator
 {
-    public string GenerateReport(IList<Product> products)
+    public string GenerateReport(IEnumerable<Product> products)
     {
-        StringBuilder stringBuilder = new StringBuilder();
+        IList<Product> productList = [.. products];
+        decimal total = productList.Sum(p => p.GetTotalPrice());
 
-        _ = stringBuilder.AppendLine("FINANCIAL REPORT");
-        _ = stringBuilder.AppendLine("================\n");
+        IList<string> reportLines =
+        [
+            "FINANCIAL REPORT",
+            "================",
+            "",
+            $"Report Date: {DateTime.Now.ToString("MMMM dd, yyyy", CultureInfo.CurrentCulture)}",
+            $"Culture: {CultureInfo.CurrentCulture.Name} ({CultureInfo.CurrentCulture.EnglishName})",
+            "",
+            string.Format("{0,-20} {1,-10} {2,-10} {3,-10}", "Product Name", "Price", "Quantity", "Total"),
+            new string('-', 50),
+            .. productList.Select(product =>
+            {
+                string priceFormatted = product.Price.ToString("C", CultureInfo.CurrentCulture);
+                string productTotal = product.GetTotalPrice().ToString("C", CultureInfo.CurrentCulture);
+                return string.Format("{0,-20} {1,-10} {2,-10} {3,-10}",
+                    product.Name, priceFormatted, product.Amount, productTotal);
+            }),
+            .. new[]
+            {
+                new string('-', 50),
+                $"Total: {total.ToString("C", CultureInfo.CurrentCulture)}"
+            },
+        ];
 
-        _ = stringBuilder.AppendLine(
-            $"Report Date: {DateTime.Now.ToString("MMMM dd, yyyy", CultureInfo.CurrentCulture)}"
-        );
-        _ = stringBuilder.AppendLine(
-            $"Culture: {CultureInfo.CurrentCulture.Name} ({CultureInfo.CurrentCulture.EnglishName})"
-        );
-        _ = stringBuilder.AppendLine();
-
-        _ = stringBuilder.AppendLine(
-            string.Format(
-                "{0,-20} {1,-10} {2,-10} {3,-10}",
-                "Product Name",
-                "Price",
-                "Quantity",
-                "Total"
-            )
-        );
-        _ = stringBuilder.AppendLine(new string('-', 50));
-
-        decimal total = products.Sum(p => p.Price * p.Amount);
-
-        foreach (Product product in products)
-        {
-            string productTotal = (product.Price * product.Amount).ToString(
-                "C",
-                CultureInfo.CurrentCulture
-            );
-            string priceFormatted = product.Price.ToString(
-                "C",
-                CultureInfo.CurrentCulture
-            );
-            _ = stringBuilder.AppendLine(
-                string.Format(
-                    "{0,-20} {1,-10} {2,-10} {3,-10}",
-                    product.Name,
-                    priceFormatted,
-                    product.Amount,
-                    productTotal
-                )
-            );
-        }
-
-        _ = stringBuilder.AppendLine(new string('-', 50));
-        _ = stringBuilder.AppendLine(
-            $"Total: {total.ToString("C", CultureInfo.CurrentCulture)}"
-        );
-
-        return stringBuilder.ToString();
+        return string.Join(Environment.NewLine, reportLines);
     }
 }
