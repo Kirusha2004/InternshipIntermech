3aff1f2e5ffec42366ae0fc2e2b7d90449bf0d33
Kirusha2004 <yodas7212@gmail.com>
2025-10-13 14:25:01 +0300

    переделал anemic класс

--- Task7/Task7/Product.cs
+++ Task7/Task7/Product.cs
@@ -2,7 +2,34 @@ namespace Task7;
 
 public class Product
 {
-    public string? Name { get; set; }
-    public decimal Price { get; set; }
-    public int Amount { get; set; }
+    public Product(string name, decimal price, int amount)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+        {
+            throw new ArgumentException("Product name cannot be empty");
+        }
+
+        if (price < 0)
+        {
+            throw new ArgumentException("Price cannot be negative");
+        }
+
+        if (amount < 0)
+        {
+            throw new ArgumentException("Amount cannot be negative");
+        }
+
+        Name = name;
+        Price = price;
+        Amount = amount;
+    }
+
+    public string Name { get; }
+    public decimal Price { get; }
+    public int Amount { get; }
+
+    public decimal GetTotalPrice()
+    {
+        return Price * Amount;
+    }
 }
