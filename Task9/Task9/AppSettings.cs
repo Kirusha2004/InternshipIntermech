87cc4e72d7778fe2f7f69f548bc00d095c78f954
Kirusha2004 <yodas7212@gmail.com>
2025-10-14 15:15:44 +0300

    Добавлены методы валидации и ApplySettings для устранения анемичной модели

--- Task9/Task9/AppSettings.cs
+++ Task9/Task9/AppSettings.cs
@@ -1,9 +1,11 @@
 using System.Windows.Media;
-
 namespace Task9;
 
 public class AppSettings
 {
+    private double _fontSize;
+    private string _fontStyle = "Normal";
+
     public AppSettings()
     {
         BackgroundColor = Colors.White;
@@ -15,7 +17,39 @@ public class AppSettings
 
     public Color BackgroundColor { get; set; }
     public Color TextColor { get; set; }
-    public double FontSize { get; set; }
-    public string FontStyle { get; set; }
+
+    public double FontSize
+    {
+        get => _fontSize;
+        set
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException("FontSize must be positive.");
+            }
+            _fontSize = value;
+        }
+    }
+
+    public string FontStyle
+    {
+        get => _fontStyle;
+        set
+        {
+            if (!_sourceArray.Contains(value))
+            {
+                throw new ArgumentException("Invalid FontStyle.");
+            }
+            _fontStyle = value;
+        }
+    }
+
     public bool UseFileConfig { get; set; }
+
+    private readonly string[] _sourceArray = ["Normal", "Italic", "Oblique"];
+
+    public bool IsValid()
+    {
+        return FontSize > 0 && !string.IsNullOrEmpty(FontStyle);
+    }
 }
